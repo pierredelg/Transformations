543daed2ebcc573d87842ddddd0058986d86f04b
delgranp <delgranp@meleze12.iut-infobio.priv.univ-lille1.fr>
2018-10-19 11:34:04 +0200

    ajout translation 2 + listenner 2

--- ModéTP1/src/transforms/Controller.java
+++ ModéTP1/src/transforms/Controller.java
@@ -29,6 +29,12 @@ public class Controller {
     @FXML
     public Slider t1y;
     
+    @FXML
+    public Slider t2x;
+
+    @FXML
+    public Slider t2y;
+    
     @FXML 
     public Slider t3;
 
@@ -38,10 +44,19 @@ public class Controller {
     @FXML
     public TextField tf1y;
     
+    @FXML
+    public TextField tf2x;
+
+    @FXML
+    public TextField tf2y;
+    
     @FXML 
     public TextField tf3;
+    
 
     private Translate t1 = new Translate(0.0, 0.0);
+    
+    private Translate t2 = new Translate(0.0, 0.0);
     
     private Scale s1 = new Scale(1,1,0,0);
     
@@ -53,6 +68,16 @@ public class Controller {
             updateT1();
         }
     };
+    
+    ChangeListener<Number> listener2 = new ChangeListener<Number>() {
+        @Override
+        public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+            tf2x.setText(Double.toString(t2x.getValue()));
+            tf2y.setText(Double.toString(t2y.getValue()));
+            updateT2();
+        }
+    };
+    
     
     ChangeListener<Number> listener3 = new ChangeListener<Number>() {
         @Override
@@ -66,6 +91,11 @@ public class Controller {
     private void updateT1() {
         t1.setX(t1x.getValue());
         t1.setY(t1y.getValue());
+    }
+    
+    private void updateT2() {
+        t2.setX(t2x.getValue());
+        t2.setY(t2y.getValue());
     }
     
     private void updateT3() {
@@ -91,10 +121,12 @@ public class Controller {
 
     @FXML
     public void setT2X() {
+    	t2x.setValue(Double.parseDouble(tf2x.getText()));
     }
 
     @FXML
     public void setT2Y() {
+    	t2y.setValue(Double.parseDouble(tf2y.getText()));
     }
 
 
@@ -129,9 +161,12 @@ public class Controller {
 
         pane.getChildren().addAll(shape1, shape2,polygon);
         shape2.getTransforms().addAll(t1,s1);
+        shape1.getTransforms().addAll(t2);
 
         t1x.valueProperty().addListener(listener1);
         t1y.valueProperty().addListener(listener1);
+        t2x.valueProperty().addListener(listener2);
+        t2y.valueProperty().addListener(listener2);
         
         t3.valueProperty().addListener(listener3);
     }
